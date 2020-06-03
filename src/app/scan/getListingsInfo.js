0b3450ea934febd99202298c8e7b8da9b155405c
Harvey1717 <harvey.woodall@me.com>
2020-06-03 15:02:22 +0100

    Added pagination support

--- src/app/scan/getListingsInfo.js
+++ src/app/scan/getListingsInfo.js
@@ -3,27 +3,32 @@ const cheerio = require('cheerio');
 module.exports = (rSes) => {
   return new Promise(async (resolve, reject) => {
     try {
-      const res = await rSes({
-        uri: 'https://restocks.eu/account/listings/resale',
-        qs: {
-          page: '1',
-        },
-        json: true,
-      });
-
-      const $ = cheerio.load(res.products);
-      const listings = $('tr.clickable');
-      
       const listingsInfo = [];
-      listings.each((i, elem) => {
-        listingsInfo.push({
-          listingPrice: $(elem).find('.storeprice__value').text(),
-          productId: $(elem).find('.productid').val(),
-          baseProductId: $(elem).find('.baseproductid').val(),
-          sizeId: $(elem).find('.sizeid').val(),
+      let pageNumber = 1;
+      while (true) {
+        const res = await rSes({
+          uri: 'https://restocks.eu/account/listings/resale',
+          qs: {
+            page: pageNumber,
+          },
+          json: true,
         });
-      });
 
+        pageNumber++;
+        if (res.products.includes('You have no advertisements yet')) break;
+
+        const $ = cheerio.load(res.products);
+        const listings = $('tr.clickable');
+
+        listings.each((i, elem) => {
+          listingsInfo.push({
+            listingPrice: $(elem).find('.storeprice__value').text(),
+            productId: $(elem).find('.productid').val(),
+            baseProductId: $(elem).find('.baseproductid').val(),
+            sizeId: $(elem).find('.sizeid').val(),
+          });
+        });
+      }
       resolve(listingsInfo);
     } catch (ex) {
       reject(ex);
