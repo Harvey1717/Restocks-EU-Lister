d265f973545d38b71867f528d5c448a3ad5e0dbb
Harvey1717 <harvey.woodall@me.com>
2020-06-03 16:27:12 +0100

    Fixed html parser issue when page number is > 1

--- src/app/scan/getListingsInfo.js
+++ src/app/scan/getListingsInfo.js
@@ -4,8 +4,9 @@ module.exports = (rSes) => {
   return new Promise(async (resolve, reject) => {
     try {
       const listingsInfo = [];
-      let pageNumber = 1;
+      let pageNumber = 0;
       while (true) {
+        pageNumber++;
         const res = await rSes({
           uri: 'https://restocks.eu/account/listings/resale',
           qs: {
@@ -14,9 +15,28 @@ module.exports = (rSes) => {
           json: true,
         });
 
-        pageNumber++;
-        if (res.products.includes('You have no advertisements yet')) resolve(listingsInfo);
-        const $ = cheerio.load(res.products);
+        let html = res.products;
+        if (html.includes('You have no advertisements yet')) return resolve(listingsInfo);
+        if (!html.includes('table')) {
+          html = `
+          <table class="listings" width="100%">
+          <thead>
+          <tr>
+          <th width="100"></th>
+          <th>Item</th>
+          <th>Price</th>
+          <th width="100">Status</th>
+          <th width="60"></th>
+          </tr>
+          </thead>
+          <tbody>
+          ${html}
+          </tbody>
+          </table>
+          </body>
+          </html>`;
+        }
+        const $ = cheerio.load(html);
         const listings = $('tr.clickable');
 
         listings.each((i, elem) => {
