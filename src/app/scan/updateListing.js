9f3052401307a65dcfe44f7d9c10174288d31641
Harvey1717 <harvey.woodall@me.com>
2020-06-04 10:42:52 +0100

    Fixed scan log number

--- src/app/scan/updateListing.js
+++ src/app/scan/updateListing.js
@@ -1,18 +1,24 @@
 const log = require('@harvey1717/logger')();
 const getCsrfToken = require.main.require('./app/getCsrfToken');
 const getPayout = require.main.require('./app/getPayout');
+const { lowestAskDifference } = require.main.require('../config/config');
 
 module.exports = (rSes, c, productId, lowestAsk) => {
   return new Promise(async (resolve, reject) => {
     try {
+      const userSetPrice = lowestAsk + lowestAskDifference;
       const res = await rSes({
         uri: 'https://restocks.eu/account/listings/edit',
         method: 'POST',
-        form: { _token: await getCsrfToken(rSes), id: productId, price: getPayout(lowestAsk, 'resale') }, //* Price = Payout
+        form: {
+          _token: await getCsrfToken(rSes),
+          id: productId,
+          price: getPayout(userSetPrice, 'resale'),
+        }, //* Price = Payout
         json: true,
       });
 
-      if (res.success) log.msuccess(c + 1, `LISTING UPDATED -> €${lowestAsk}`);
+      if (res.success) log.msuccess(c, `LISTING UPDATED -> €${userSetPrice}`);
       else log.error(`LISTING ERROR -> SUCCESS "${success}"`);
       resolve();
     } catch (ex) {
