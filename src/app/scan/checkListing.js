80ab7dadb9953cdc46c438efa0cf90aaafb87d08
Harvey1717 <harvey.woodall@me.com>
2020-06-05 16:25:51 +0100

    Added percentage decrease checking

--- src/app/scan/checkListing.js
+++ src/app/scan/checkListing.js
@@ -12,6 +12,8 @@ module.exports = (rSes, c, listing) => {
       if (res == 0) throw new Error('Lowest ask is 0');
 
       if (parseFloat(res) < parseFloat(listingPrice)) {
+        const percentageDecrease = ((parseFloat(listingPrice) - parseFloat(res)) / parseFloat(listingPrice)) * 100;
+        if (percentageDecrease >= 5) throw new Error('New listing price is more than 5% lower than current');
         log.mwarn(c, `LISTING PRICE [€${listingPrice}] IS HIGHER THAN LOWEST ASK [€${res}]`);
         resolve({ changeNeeded: true, lowestAsk: res });
       } else {
