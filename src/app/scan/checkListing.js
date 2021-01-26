cba6bceb484a851456e642ef233efaa80713ad22
mattia.git <mattia.salsi@hotmail.com>
2021-01-26 16:56:57 +0000

    Remove 5% check

--- src/app/scan/checkListing.js
+++ src/app/scan/checkListing.js
@@ -13,13 +13,14 @@ module.exports = (rSes, c, listing) => {
       if (res == 0) throw new Error('Lowest ask is 0');
 
       if (whitelistedProducts.includes(productName)) {
-        log.mlog(productId, 'PRODUCT WHITELISTED')
-        resolve({changeNeeded: false});
+        log.mlog(productId, 'PRODUCT WHITELISTED');
+        resolve({ changeNeeded: false });
+        return;
       }
 
       if (parseFloat(res) < parseFloat(listingPrice)) {
         const percentageDecrease = ((parseFloat(listingPrice) - parseFloat(res)) / parseFloat(listingPrice)) * 100;
-        if (percentageDecrease >= 5) throw new Error('New listing price is more than 5% lower than current');
+        //if (percentageDecrease >= 5) throw new Error('New listing price is more than 5% lower than current');
         log.mwarn(productId, `LISTING PRICE [€${listingPrice}] IS HIGHER THAN LOWEST ASK [€${res}]`);
         resolve({ changeNeeded: true, lowestAsk: res });
       } else if (parseFloat(res) > parseFloat(listingPrice)) {
